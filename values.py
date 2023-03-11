6e5d133dda92b1ad46cfc3c3243191104ca28e7c
Erik Bohne <eriknbohne@gmail.com>
2023-03-11 15:44:11 +0100

    11march23

--- values.py
+++ values.py
@@ -4,31 +4,124 @@ LEAGUES = [
       "id" : 47,
       "teams" : [
                     {   
-                        "name" : "Manchester United",
-                        "short" : "Man Utd",
-                        "UrlName" : "manchester-united",
-                        "id" : 10260
-                    },
-                    {
                         "name" : "Arsenal",
                         "short" : "Arsenal",
                         "UrlName" : "arsenal",
                         "id" : 9825
                     },
                     {
-                        "name" : "TEAMNAME",
-                        "short" : "TEAMNAMESHORT",
-                        "id" : "TEAMID"
+                        "name" : "Manchester City",
+                        "short" : "Man City",
+                        "UrlName" : "manchester-city",
+                        "id" : 8456
                     },
                     {
-                        "name" : "TEAMNAME",
-                        "short" : "TEAMNAMESHORT",
-                        "id" : "TEAMID"
+                        "name" : "Manchester United",
+                        "short" : "Man Utd",
+                        "UrlName" : "manchester-united",
+                        "id" : 10260
                     },
                     {
-                        "name" : "TEAMNAME",
-                        "short" : "TEAMNAMESHORT",
-                        "id" : "TEAMID"
+                        "name" : "Tottenham Hotspur",
+                        "short" : "Tottenham",
+                        "UrlName" : "tottenham-hotspur",
+                        "id" : 8586
+                    },
+                    {
+                        "name" : "Liverpool",
+                        "short" : "Liverpool",
+                        "UrlName" : "liverpool",
+                        "id" : 8650
+                    },
+                    {
+                        "name" : "Newcastle United",
+                        "short" : "Newcastle",
+                        "UrlName" : "newcastle-united",
+                        "id" : 10261
+                    },
+                    {
+                        "name" : "Fulham",
+                        "short" : "Fulham",
+                        "UrlName" : "fulham",
+                        "id" : 9879
+                    },
+                    {
+                        "name" : "Brighton & Hove Albion",
+                        "short" : "Brighton",
+                        "UrlName" : "brighton--hove-albion",
+                        "id" : 10204
+                    },
+                    {
+                        "name" : "Brentford",
+                        "short" : "Brentford",
+                        "UrlName" : "brentford",
+                        "id" : 9937
+                    },
+                    {
+                        "name" : "Chelsea",
+                        "short" : "Chelsea",
+                        "UrlName" : "chelsea",
+                        "id" : 8455
+                    },
+                    {
+                        "name" : "Aston Villa",
+                        "short" : "Aston Villa",
+                        "UrlName" : "aston-villa",
+                        "id" : 10252
+                    },
+                    {
+                        "name" : "Crystal Palace",
+                        "short" : "Crystal Palace",
+                        "UrlName" : "crystal-palace",
+                        "id" : 9826
+                    },
+                    {
+                        "name" : "Wolverhampton Wanderers",
+                        "short" : "Wolves",
+                        "UrlName" : "wolverhampton-wanderers",
+                        "id" : 9879
+                    },
+                    {
+                        "name" : "Nottingham Forest",
+                        "short" : "Nottm Forest",
+                        "UrlName" : "nottingham-forest",
+                        "id" : 10203
+                    },
+                    {
+                        "name" : "Leicester City",
+                        "short" : "Leicester",
+                        "UrlName" : "leicester-city",
+                        "id" : 8197
+                    },
+                    {
+                        "name" : "AFC Bournemouth",
+                        "short" : "Bournemouth",
+                        "UrlName" : "afc-bournemouth",
+                        "id" : 8678
+                    },
+                    {
+                        "name" : "West Ham United",
+                        "short" : "West Ham",
+                        "UrlName" : "west-ham-united",
+                        "id" : 8654
+                    },
+                    {
+                        "name" : "Leeds United",
+                        "short" : "Leeds",
+                        "UrlName" : "leeds-united",
+                        "id" : 8463
+                    },
+                    {
+                        "name" : "Everton",
+                        "short" : "Everton",
+                        "UrlName" : "everton",
+                        "id" : 8668
+                    },
+                    {
+                        "name" : "Southampton",
+                        "short" : "Southampton",
+                        "UrlName" : "southampton",
+                        "id" : 8466
                     }
                 ]
     },
