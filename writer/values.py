0c2f48265cac03e36c7839632fb6e35da0067c2e
Erik Bohne <eriknbohne@gmail.com>
2023-03-25 09:04:27 +0100

    leagues info in json file

---
+++ writer/values.py
@@ -0,0 +1,15 @@
+MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+notIncluded = ["<", ">", "/html", "/div", "/body",
+               "{", "}", ":", "''", "scriptLoader",
+               "[", "]", "true", "appGip", ",",
+               "false", "id", "tab", "/", "shortName",
+               "``", "div", "by-round", "isFallback", "gssp",
+               "customServer", "id=", "/script", "slug", "buildId",
+               "modal-root", "name", "...", "page", "isMobile", "/leagues/",
+               ";", "@", "null", "ssr", "/match/", "--", "/span",
+               "/footer", "--", "!", "class=", "https", "(", ")", "meta", "content=",
+               "title", "/title", ".", "DOCTYPE", "html", "en", "head", "name=",
+               "href=", "property=", "charset=", "?", "lang=", "link", "rel=", "fb",
+               "script", "defer=", "src="]
