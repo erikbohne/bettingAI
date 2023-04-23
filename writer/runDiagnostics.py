edd8747808ceaeb50a8d8f651688cf31b7e9406c
Erik Bohne <eriknbohne@gmail.com>
2023-04-23 13:04:23 +0200

    program to run a diagnosis on what causes errors

---
+++ writer/runDiagnostics.py
@@ -0,0 +1,83 @@
+"""
+The goal of this program is to check what matches and players are added to the database and which are not.
+To better understand why some dont get added.
+"""
+import os
+import sys
+
+sys.path.append(os.path.join("..", "googleCloud"))
+from initPostgreSQL import initPostgreSQL
+
+from google.cloud.sql.connector import Connector
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import and_
+import traceback
+
+from databaseClasses import *
+from addRow import *
+from scraper import get_match_links, get_player_links, get_player_bio
+from values import SEASONS
+
+def main(session):
+    
+    # Gather a list of the league ids
+    leagues = session.query(Leagues).all()
+    leagueIDs = [(league.id, league.year_span) for league in leagues]
+    
+    notInDatabase = {"matches" : {}, "players" : {}}
+    
+    for id, span in leagueIDs: # iterate over each league
+        
+        notInDatabase["matches"][id] = {}
+        notInDatabase["players"][id] = {}
+        
+        for season in SEASONS[span]: # iterate through each season
+            
+            matches = [int(link.split("/")[2]) for link in get_match_links(id, season)]
+            database = session.query(Matches).filter(and_(Matches.league_id == id, Matches.season == season))
+            database = set([match.id for match in database])
+            
+            notInDatabase["matches"][id][season] = []
+            for match in matches:
+                if match not in database:
+                    notInDatabase["matches"][id][season].append(match)
+        
+        teams = session.query(Teams).filter(Teams.league_id == id)
+        teams = [team.id for team in teams]
+        
+        for team in teams: # iterate over each team
+            continue
+            players = [int(link.split("/")[2]) for link in get_player_links(team)]
+            database = session.query(Players).filter(Players.team_id == team)
+            database = set([player.id for player in database])
+
+            notInDatabase["players"][id][team] = {}
+            for player in players:
+                if player not in database:
+                    notInDatabase["players"][id][team][player] = str()
+                    try:
+                        session.add(add_player_bio(player, team, "David Moyes", get_player_bio(f"/players/78626")))
+                        session.commit()
+                    except Exception as e:
+                        session.rollback()
+                        tb_str = traceback.format_exception(type(e), e, e.__traceback__)
+                        print(player)
+                        print("".join(tb_str))
+                        exit()
+                        notInDatabase["players"][id][team][player] = e
+            
+            print(notInDatabase["players"][id][team])
+    
+    print(notInDatabase["matches"][47]["2020-2021"])
+            
+            
+    
+if __name__ == "__main__":
+    
+    connection = initPostgreSQL()
+
+    # Start session with connection
+    Session = sessionmaker(connection)
+    session = Session()
+    
+    main(session)
