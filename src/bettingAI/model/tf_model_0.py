e93542b5d0ca1d36fd616f0ebadf8296e994bca9
Erik Bohne <eriknbohne@gmail.com>
2023-05-11 09:24:51 +0200

    model directory clean-up

--- src/bettingAI/model/tf_model_0.py
+++ src/bettingAI/model/tf_model_0.py
@@ -2,21 +2,24 @@
 Tensorflow model to predict the probability of 8 scenarios
 [win, draw, loss, over 2.5, over 3.5, 4.5, 5.5, btts]
 """
-from sqlalchemy import text
+from sqlalchemy import text, and_
 from bettingAI.googleCloud.initPostgreSQL import initSession
+from bettingAI.writer.databaseClasses import Processed, Matches, Leagues
 
 import numpy as np
+from datetime import datetime
 from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, Dropout, Concatenate
+from tensorflow.keras.layers import Input, Dense, Dropout, BatchNormalization
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.models import Model
 
 import matplotlib.pyplot as plt
 
 # HYPERPARAMETERS
 BATCH_SIZE = 32
-EPOCHS = 20
+EPOCHS = 80
 
 def main():
     X_train, X_test, y_train, y_test = load_data()
@@ -27,38 +30,62 @@ def main():
     print(f"X_test shape: {X_test.shape}")
     print(f"y_test shape: {y_test.shape}")
     
-    model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=1)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+    model_checkpoint = ModelCheckpoint('models/best_model.h5', save_best_only=True, monitor='val_loss', mode='min')
+    
+    callbacks = [early_stopping, model_checkpoint]
+    
+    model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=1, validation_split=0.2, callbacks=callbacks)
     scores = model.evaluate(X_test, y_test, verbose=1)
     print("Test loss: ", scores[0])
     print("Test accuracy: ", scores[1])
     
-    predict_matches(model)
+    #predict_matches(model)
     # Save the trained model as an .h5 file
-    #model.save('model/model3.h5')
+    model.save('models/model4.h5')
     
 def load_data():
     session = initSession()
     matches = session.execute(text("SELECT inputs, labels FROM processed_for_model0"))
+    
+    year = 2023
+    exclude_league_id = 47
+
+    query = text(f"""
+        SELECT p.inputs, p.labels
+        FROM processed_for_model0 p
+        JOIN matches m ON p.match_id = m.id
+        JOIN leagues l ON m.league_id = l.id
+        WHERE NOT (l.id = :exclude_league_id AND m.date >= :start_date AND m.date < :end_date)
+    """)
+
+    matches = session.execute(query, {
+        'exclude_league_id': exclude_league_id,
+        'start_date': datetime(year, 1, 29),
+        'end_date': datetime(year + 1, 1, 1)
+    })
+
     data, labels = zip(*[(match[0], [match[1][0], match[1][1], match[1][2]]) for match in matches])
     
     data, labels = np.array(data), np.array(labels)
     
     print(f"Data shape: {data.shape}")
     print(f"Labels shape: {labels.shape}")
-    
-    return train_test_split(data, labels, test_size=0.2, random_state=42)
+
+    return train_test_split(data, labels, test_size=0.2)
     
 
 def bettingAI_model0():
     input_layer = Input(shape=(42,))
     
-    hidden_1 = Dense(128, activation='relu')(input_layer)
-    dropout_1 = Dropout(0.2)(hidden_1)
+    hidden_1 = Dense(64, activation='relu')(input_layer)
+    batch_norm_1 = BatchNormalization()(hidden_1)
+    dropout_1 = Dropout(0.2)(batch_norm_1)
     
-    hidden_2 = Dense(64, activation='relu')(dropout_1)
+    hidden_2 = Dense(32, activation='relu')(dropout_1)
     dropout_2 = Dropout(0.2)(hidden_2)
     
-    hidden_3 = Dense(32, activation='relu')(dropout_2)
+    hidden_3 = Dense(16, activation='relu')(dropout_2)
     
     output_layer = Dense(3, activation='softmax')(hidden_3)
 
@@ -66,22 +93,6 @@ def bettingAI_model0():
     model.summary()
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
-
-def predict_matches(model):
-    session = initSession()
-    match_ids = [3901207, 3901208, 3901262, 3901268, 3901269]
-    
-    for match_id in match_ids:
-        # Retrieve the input features for the match
-        match = session.execute(text("SELECT inputs FROM processed_for_model0 WHERE match_id=:match_id"), {"match_id": match_id}).first()
-
-        if match:
-            input_data = np.array(match[0]).reshape(1, -1)  # Reshape the input data into the expected format
-            probabilities = model.predict(input_data)  # Get the predicted probabilities for the match
-            real_odds = 1 / probabilities
-            print(f"Match ID {match_id}: {probabilities} and oddds {real_odds}")
-        else:
-            print(f"Match ID {match_id} not found.")
 
     
 if __name__ == "__main__":
---
+++ src/bettingAI/model/tf_model_0_1.py
@@ -0,0 +1,129 @@
+import numpy as np
+from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold
+from sklearn.inspection import permutation_importance
+
+from sqlalchemy import text, and_
+from bettingAI.googleCloud.initPostgreSQL import initSession
+from bettingAI.writer.databaseClasses import Processed, Matches, Leagues
+
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense, Dropout, BatchNormalization
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.models import Model
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+
+import matplotlib.pyplot as plt
+
+# HYPERPARAMETERS
+BATCH_SIZE = 32
+EPOCHS = 80
+N_SPLITS = 5
+
+def main():
+    X_train, X_test, y_train, y_test = load_data()
+    X_train, X_test = normalize_data(X_train, X_test)
+
+    # K-Fold Cross Validation
+    kf = KFold(n_splits=N_SPLITS, shuffle=True, random_state=42)
+    val_loss = []
+    val_accuracy = []
+
+    for train_index, val_index in kf.split(X_train):
+        X_train_kf, X_val_kf = X_train[train_index], X_train[val_index]
+        y_train_kf, y_val_kf = y_train[train_index], y_train[val_index]
+
+        early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+        model_checkpoint = ModelCheckpoint('models/best_model.h5', save_best_only=True, monitor='val_loss', mode='min')
+
+        callbacks = [early_stopping, model_checkpoint]
+
+        wrapped_model = KerasClassifier(build_fn=bettingAI_model0)
+        wrapped_model.fit(X_train_kf, y_train_kf, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=1, validation_data=(X_val_kf, y_val_kf), callbacks=callbacks)
+        scores = wrapped_model.model.evaluate(X_val_kf, y_val_kf, verbose=1)
+        val_loss.append(scores[0])
+        val_accuracy.append(scores[1])
+
+    print(f"Average validation loss: {np.mean(val_loss)}")
+    print(f"Average validation accuracy: {np.mean(val_accuracy)}")
+
+    wrapped_model.model.load_weights('models/best_model.h5')
+    scores = wrapped_model.model.evaluate(X_test, y_test, verbose=1)
+    print("Test loss: ", scores[0])
+    print("Test accuracy: ", scores[1])
+
+    # Feature Importance
+    feature_importance(wrapped_model, X_test, y_test)
+
+    # Save the trained model as an .h5 file
+    wrapped_model.model.save('models/test.h5')
+
+def load_data():
+    session = initSession()
+    matches = session.execute(text("SELECT inputs, labels FROM processed_for_model0"))
+    
+    year = 2023
+    exclude_league_id = 47
+
+    query = text(f"""
+        SELECT p.inputs, p.labels
+        FROM processed_for_model0 p
+        JOIN matches m ON p.match_id = m.id
+        JOIN leagues l ON m.league_id = l.id
+        WHERE NOT (l.id = :exclude_league_id AND m.date >= :start_date AND m.date < :end_date)
+    """)
+
+    matches = session.execute(query, {
+        'exclude_league_id': exclude_league_id,
+        'start_date': datetime(year, 1, 29),
+        'end_date': datetime(year + 1, 1, 1)
+    })
+
+    data, labels = zip(*[(match[0], [match[1][0], match[1][1], match[1][2]]) for match in matches])
+    
+    data, labels = np.array(data), np.array(labels)
+    
+    print(f"Data shape: {data.shape}")
+    print(f"Labels shape: {labels.shape}")
+    return train_test_split(data, labels, test_size=0.2)
+
+def normalize_data(X_train, X_test):
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    return X_train, X_test
+
+def bettingAI_model0():
+    input_layer = Input(shape=(42,))
+    
+    hidden_1 = Dense(64, activation='relu')(input_layer)
+    batch_norm_1 = BatchNormalization()(hidden_1)
+    dropout_1 = Dropout(0.2)(batch_norm_1)
+    
+    hidden_2 = Dense(32, activation='relu')(dropout_1)
+    dropout_2 = Dropout(0.2)(hidden_2)
+    
+    hidden_3 = Dense(16, activation='relu')(dropout_2)
+    
+    output_layer = Dense(3, activation='softmax')(hidden_3)
+
+    model = Model(inputs=input_layer, outputs=output_layer)
+    model.summary()
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+def feature_importance(wrapped_model, X_test, y_test):
+    y_test_class_indices = np.argmax(y_test, axis=1)
+    result = permutation_importance(wrapped_model, X_test, y_test_class_indices, scoring='accuracy', n_repeats=10, random_state=42, n_jobs=1)
+    sorted_idx = result.importances_mean.argsort()
+
+    plt.figure(figsize=(12, 6))
+    plt.barh(range(X_test.shape[1]), result.importances_mean[sorted_idx])
+    plt.yticks(range(X_test.shape[1]), sorted_idx)
+    plt.xlabel('Permutation Importance')
+    plt.show()
+    
+if __name__ == "__main__":
+    main()
